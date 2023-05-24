87ec98a6fc3a8a2b34eb9f048c609375bb0d3049
Emlyn Matheson <emlyn@emlyn.xyz>
2023-05-25 03:13:36 +0959

    Explicity include font

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-function cmdsub() {
+cmd_input = document.getElementsByTagName("input")[0];
+
+COLOR_HIDDEN = "#454850"
+COLOR_SHOWN = "#e2e2e2";
+
+CUSTOM_URL = "custom_url";
+
+
+function cmd_submit() {
 	idx = setLinkVis();
 	if (idx) {
 		window.location.href = document.getElementById("links").children[idx].dataset["url"];
 	}
 }
 
-COLOR_HIDDEN = "#454850"
-COLOR_SHOWN = "#e2e2e2";
-
-function okd(event) {
-	if (event.keyCode == 13) { cmdsub() }
+function onkeydown(event) {
+	if (event.keyCode == 13) { cmd_submit() }
 }
 
-function ob() {
+function onblur() {
 	setTimeout(function() {document.getElementsByTagName('input')[0].focus()}, 0);
 }
 
 function setLinkVis() {
 
-	txt = document.getElementsByTagName("input")[0].value;
+	cmd = cmd_input.value;
 
 	c = 0;
 	idx = -1;
 
 	links = document.getElementById("links").children;
 	for (i=3; i < links.length; i++) {
-		if (links[i].innerHTML.toLowerCase().includes(txt.toLowerCase())) {
+		if (links[i].innerHTML.toLowerCase().includes(cmd.toLowerCase())) {
 			links[i].style.color = COLOR_SHOWN;
 			c++;
 			idx = i;
@@ -38,8 +43,18 @@ function setLinkVis() {
 
 
 window.addEventListener("load", (e) => {
+
+
 	inp = document.getElementsByTagName("input")[0];
-	inp.onblur = (ev) => { ob()};
+	inp.onblur = (ev) => { onblur()};
 	inp.oninput = (ev) => { setLinkVis() };
-	inp.onkeydown = (ev) => {okd(ev)};
+	inp.onkeydown = (ev) => { onkeydown(ev)};
+	setFocus();
+
+
 });
+
+function setFocus() {
+	cmd_input.focus();
+	setTimeout(setFocus, 100);
+};
